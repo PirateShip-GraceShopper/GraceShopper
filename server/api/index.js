6a4dd5807bb3aa82b2a3b9b68e688b92c2f88608
James O'Reilly <James.Oreilly926@gmail.com>
2017-10-30 17:56:12 -0400

    feat create review backend routes

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 module.exports = router
+const reviews = require('./reviews')
 
 router.use('/users', require('./users'))
+router.use('/reviews', reviews)
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
