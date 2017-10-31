ecc343c3aad09772e8bdb3ff011b4864e224207a
Mueed Chaudhry <mueed.chaudhry2@gmail.com>
2017-10-31 17:16:24 -0400

    fix(product): bugs found and squashed

--- client/components/productlist.js
+++ client/components/productlist.js
@@ -1,29 +1,21 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
-import { products } from '../store'
+import React, { Component } from "react"
+import { connect } from "react-redux"
+import { Link } from "react-router-dom"
+import { products } from "../store"
 
-const ProductList = ({ products }) => {
+const ProductList = ({ products }) => (
+  <div className="container">
+    <h2>Shoes</h2>
+    <ul className="list-group">
+      {products &&
+        products.map(product => (
+          <ProductItem key={product.id} product={product} />
+        ))}
+    </ul>
+  </div>
+)
 
-  render () {
-      return (
-        <div className="container">
-          <br />
-          <h2>Shoes</h2>
-          <ul className="list-group">
-          {
-            products &&
-            products.map(product =>
-              <ProductItem key={product.id} product={product} />
-              )
-          }
-          </ul>
-        </div>
-        )
-    }
-}
-
-const mapState = ({ products }) => ({ products })
+const mapState = ({ products }) => ({ products });
 const mapDispatch = null;
 
-export default connect(mapState, mapDispatch)(ProductList)
+export default connect(mapState, mapDispatch)(ProductList);
