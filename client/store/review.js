e86a6401a224761deb5c715835f0b06c507a6885
James O'Reilly <James.Oreilly926@gmail.com>
2017-10-31 12:42:59 -0400

    feat(review-store): finsh action creators, reducer

--- client/store/review.js
+++ client/store/review.js
@@ -1,1 +1,34 @@
+import axios from 'axios';
 
+const GET_REVIEW = 'GET_REVIEW';
+const POST_REVIEW = 'POST_REVIEW';
+const REMOVE_REVIEW = 'REMOVE_REVIEW';
+
+const getReviews = reviews => ({
+    type: GET_REVIEW,
+    reviews
+})
+
+const postReview = newReview => ({
+    type: POST_REVIEW,
+    newReview
+})
+
+const removeReview = targetReview => ({
+    type: REMOVE_REVIEW,
+    targetReview
+})
+
+
+const initialState = { reviews: [] }
+
+export default const reducer = (prevState = initialState, action) => {
+    switch (action.type) {
+        case GET_REVIEWS:
+            return Object.assign({}, prevState, {reviews: action.reviews})
+        case POST_REVIEW:
+            return Object.assign({}, prevState, {reviews: prevState.reviews.concat(action.reviews)})
+        default:
+            return prevState
+    }
+}
