b00c1089ade94e1506b277d30763ac8710db17a3
Mueed Chaudhry <mueed.chaudhry2@gmail.com>
2017-10-31 15:17:28 -0400

    fix(product): store reducer

--- client/components/productlist.js
+++ client/components/productlist.js
@@ -1,4 +1,24 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
-import store from '../store'
+import { products, fetchProducts } from
+
+class ProductList extends Component {
+
+  render() {
+    const { products } = this.props
+    return (
+      <div>
+        <h2>Shoes</h2>
+        {
+
+        }
+      </div>
+      )
+  }
+}
+
+const mapState = ({ products }) => ({ products })
+const mapDispatch = { fetchProducts }
+
+export default connect(mapState, mapDispatch)(ProductList)
