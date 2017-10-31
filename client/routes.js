c45884078c9cf09d135bfe65f92d21300a950fdd
Mueed Chaudhry <mueed.chaudhry2@gmail.com>
2017-10-31 17:08:26 -0400

    changing file name to capital

--- client/routes.js
+++ client/routes.js
@@ -33,8 +33,8 @@ class Routes extends Component {
                 <Switch>
                   {/* Routes placed here are only available after logging in */}
                   <Route path="/home" component={UserHome} />
-                  <Route path="/products" component={ProductList} />
                   <Route path="/edit_profile" component={SingleUser}/>
+                  <Route path="/products" component={ProductList} />
                 </Switch>
             }
             {/* Displays our Login component as a fallback */}
