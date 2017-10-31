1ffaacf1cba0cd87b8a0886ea395458dbd4cad89
Mueed Chaudhry <mueed.chaudhry2@gmail.com>
2017-10-31 16:43:35 -0400

    feat(product): route added when logged in

--- client/routes.js
+++ client/routes.js
@@ -31,6 +31,7 @@ class Routes extends Component {
                 <Switch>
                   {/* Routes placed here are only available after logging in */}
                   <Route path="/home" component={UserHome} />
+                  <Route path="/products" component={ProductsList} />
                 </Switch>
             }
             {/* Displays our Login component as a fallback */}
