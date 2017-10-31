3ffbe3103e94c0744d01a45eaf77589a6a6782ba
Mueed Chaudhry <mueed.chaudhry2@gmail.com>
2017-10-31 15:47:42 -0400

    feat(product): products rendered

--- client/components/productlist.js
+++ client/components/productlist.js
@@ -5,23 +5,23 @@ import { products, fetchProducts } from '../store'
 
 class ProductList extends Component {
 
-  render() {
-    const { products } = this.props
-    return (
-      <div className="container">
-        <br />
-        <h2>Shoes</h2>
-        <ul className="list-group">
-        {
-          products &&
-          products.map(product =>
-            <ProductItem key={product.id} product={product} />
-            )
-        }
-        </ul>
-      </div>
-      )
-  }
+  render () {
+      const { products } = this.props
+      return (
+        <div className="container">
+          <br />
+          <h2>Shoes</h2>
+          <ul className="list-group">
+          {
+            products &&
+            products.map(product =>
+              <ProductItem key={product.id} product={product} />
+              )
+          }
+          </ul>
+        </div>
+        )
+    }
 }
 
 const mapState = ({ products }) => ({ products })
