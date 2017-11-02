557b4400ca978889ea6f9f35c93af1114c3941ce
James O'Reilly <James.Oreilly926@gmail.com>
2017-11-02 12:38:00 -0400

    feat: ReviewStars component complete

--- client/components/ReviewStars.js
+++ client/components/ReviewStars.js
@@ -4,20 +4,21 @@ import React, {Component} from 'react'
 export default class ReviewStars extends Component {
     constructor() {
         super()
+        this.ratingChanged = this.ratingChanged.bind(this)
     }
 
     ratingChanged(newRating) {
-        console.log(newRating);
+        
     }
 
     render() {
         return (
             <div>
                 <ReactStars
-                    count={5},
-                    onChange={ratingChanged},
-                    size={24},
-                    color2={'#ffd700'} 
+                    count={5}
+                    onChange={this.ratingChanged}
+                    size={24}
+                    color2={'#ffd700'}
                 />
             </div>
         )
