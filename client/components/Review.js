e3fa9547314bb4b2b77f07f9230164bb6fbe3bd5
James O'Reilly <James.Oreilly926@gmail.com>
2017-11-03 19:05:18 -0400

    create style.css

--- client/components/Review.js
+++ client/components/Review.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {Rate} from 'antd';
 
 const Review = props => {
     const content = props.newReview.content;
     const rating = props.newReview.rating;
 
     return (
-        <div>
-            {review.content}
+        <div className="review-container">
+            <div>
+            <Rate
+                value={rating}
+                diabled={true}
+            />
+            </div>
+            <div>
+                {content}
+            </div>
         </div>
     )
 }
