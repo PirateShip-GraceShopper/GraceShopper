3db8ab63496165db157d5d244b441fd251859892
Kenny Diaz <kend77@gmail.com>
2017-11-06 15:55:38 -0500

    add cart icon, add new favicon

--- client/components/main.js
+++ client/components/main.js
@@ -30,18 +30,12 @@ const Main = ({ children, handleClick, isLoggedIn, cart }) => (
                 <Button>
                   <Link to={`/edit_profile`}>Edit Profile</Link>
                 </Button>
-                <Button ghost>
-                  <Link to="/cart">Cart</Link>
-                </Button>
                 <Button>
                   <a href="#" onClick={handleClick}>Logout</a>
                 </Button>
               </Menu.Item>
               : <Menu.Item>
                 {/* The navbar will show these links before you log in */}
-                <Button ghost>
-                  <Link to="/cart">Cart</Link>
-                </Button>
                 <Button ghost>
                   <Link to="/login">Login</Link>
                 </Button>
