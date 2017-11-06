5444308dbbc638b458744cbf24a8e936b6d01b6c
Mueed Chaudhry <mueed.chaudhry2@gmail.com>
2017-11-06 11:32:42 -0500

    feat(stripe): checkout form routes added for cart

--- client/components/CheckoutForm.js
+++ client/components/CheckoutForm.js
@@ -1,16 +1,11 @@
-import React, {Component} from 'react'
-import {Elements} from 'react-stripe-elements'
-
+import React from 'react'
+import { Elements } from 'react-stripe-elements'
 import Cart from './Cart'
 
-class CheckoutForm extends Component {
-  render() {
-    return (
-      <Elements>
-        <Cart />
-      </Elements>
-    )
-  }
-}
+const CheckoutForm = _ => (
+  <Elements>
+    <Cart />
+  </Elements>
+)
 
 export default CheckoutForm
