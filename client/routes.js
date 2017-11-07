993c9f585375d3843382fef315c80682d2dff1e2
Mueed Chaudhry <mueed.chaudhry2@gmail.com>
2017-11-07 11:31:01 -0500

    stripe forms building process

--- client/routes.js
+++ client/routes.js
@@ -4,7 +4,7 @@ import {Router} from 'react-router'
 import {Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import history from './history'
-import {Main, Login, Signup, UserHome, SingleUser, SingleItem, AllItems, ProductList, ReviewList, Cart, ProductDetail, AllUsers, CheckoutForm} from './components'
+import {Main, Login, Signup, UserHome, SingleUser, SingleItem, AllItems, ProductList, ReviewList, Cart, ProductDetail, AllUsers, StoreCheckout} from './components'
 import {me, fetchProducts, fetchAllItems, fetchReviewsThunk, fetchSessionCart} from './store'
 
 
@@ -31,7 +31,8 @@ class Routes extends Component {
             <Route path="/products/:id" component={ProductDetail} />
             <Route path="/all-items" component={AllItems} />
             <Route path="/all-reviews" component={ReviewList} />
-            <Route path="/cart" component={CheckoutForm} />
+            {/* <Route path="/cart" component={CheckoutForm} /> */}
+            <Route path="/checkout" component={StoreCheckout} />
             {
               isLoggedIn &&
                 <Switch>
@@ -40,7 +41,7 @@ class Routes extends Component {
                   <Route path="/edit_profile" component={SingleUser}/>
                   <Route path="/products" component={ProductList} />
               {
-                isAdmin && 
+                isAdmin &&
                 <Route path="/all_users" component={AllUsers} />
               }
                 </Switch>
