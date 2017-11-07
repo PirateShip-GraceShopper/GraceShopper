655761851bf56a29d2ca9088a5171fc6f72d5638
Sol <sppark95@hotmail.com>
2017-11-07 14:10:14 -0500

    NodeMailer implement to send password request email to user from ManageUsers

--- client/routes.js
+++ client/routes.js
@@ -4,7 +4,7 @@ import {Router} from 'react-router'
 import {Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import history from './history'
-import {Main, Login, Signup, UserHome, SingleUser, SingleItem, AllItems, ProductList, ReviewList, Cart, ProductDetail, AllUsers, StoreCheckout} from './components'
+import {Main, Login, Signup, UserHome, SingleUser, SingleItem, AllItems, ProductList, ReviewList, Cart, ProductDetail, AllUsers, StoreCheckout, PasswordForm} from './components'
 import {me, fetchProducts, fetchAllItems, fetchReviewsThunk, fetchSessionCart} from './store'
 
 
@@ -40,6 +40,7 @@ class Routes extends Component {
                   <Route path="/home" component={ProductList} />
                   <Route path="/edit_profile" component={SingleUser}/>
                   <Route path="/products" component={ProductList} />
+                  <Route path="/password_reset" component={PasswordForm} />
               {
                 isAdmin &&
                 <Route path="/all_users" component={AllUsers} />
